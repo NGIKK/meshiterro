b3e364f37b6da1346183352a5a02a60c8624764c
NGIKK <129597259+NGIKK@users.noreply.github.com>
2023-04-08 11:48:40 +0000

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   #end
   
   resources :post_images,only: [:new,:create,:index,:show,:destroy]
-  resources :users,only: [:show,:edit]
+  resources :users,only: [:show,:edit,:update]
   # get 'postimages/new'
   # get 'postimages/index'
   # get 'postimages/show'
